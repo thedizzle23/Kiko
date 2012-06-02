ebcd4d3451b0814edc69355ab8f4d6995d4d6bfb
Robert DeNicola <thedizzle23@gmail.com>
2012-06-02 18:28:53 -0400

    seaweed

---
+++ PJS/PJSTests/PJSTests.h
@@ -0,0 +1,13 @@
+//
+//  PJSTests.h
+//  PJSTests
+//
+//  Created by Robert DeNicola on 6/2/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PJSTests : SenTestCase
+
+@end
---
+++ PJS/PJSTests/PJSTests.m
@@ -0,0 +1,32 @@
+//
+//  PJSTests.m
+//  PJSTests
+//
+//  Created by Robert DeNicola on 6/2/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "PJSTests.h"
+
+@implementation PJSTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PJSTests");
+}
+
+@end
